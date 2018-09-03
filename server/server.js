22faf2b19e3c6b38721ee4de5cce13ab16eaa100
xdea <tjsqoo@gmail.com>
2018-09-03 12:10:03 +0900

    load info

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ app.use(express.static('../client/'));
 // request
 var request = require('request');
 const API_URL = 'https://api.upbit.com/';
+const RAISE_RATE = 3.0;
 
 // value
 var marketList = [];
@@ -20,7 +21,7 @@ var fsmStatus = startStatus.INIT;
 startFSM();
 
 function startFSM() {
-  console.log(fsmStatus);
+  // console.log(fsmStatus);
   switch (fsmStatus++) {
     case startStatus.INIT: {
       initAllData();
@@ -63,12 +64,103 @@ function loadMarketInfo() {
   });
 }
 
-function completeLoadInfo() {
-  for (var i in marketList) {
-    console.log(marketList[i]);
-  }
+var currentMarket = 0;
 
-  console.log(marketList.length);
+function completeLoadInfo() {
+
+  loadLogofMonthRaiseCount(marketList[currentMarket].market, null);
+  // var options = { 
+  //   method: 'GET',
+  //   url: API_URL + 'v1/candles/minutes/1',
+  //   qs: { 
+  //     market: marketList[0].market,
+  //     count: 200
+  //   } 
+  // };
+
+  // request(options, (error, response, body) => {
+  //   if (error) throw new Error(error);
+
+  //   var data = JSON.parse(body);
+    
+  //   for (var i in data) {
+  //     var open = data[i].opening_price;
+  //     var finish = data[i].trade_price;
+
+  //     if (finish - open > 0) {
+  //       var result = (finish - open) / open * 100;
+        
+  //       if (result > 1) {
+  //         console.log(result);
+  //         console.log(data[i].candle_date_time_kst);
+  //       }
+  //     }
+  //   }
+  //   //console.log(data);
+    
+  //   console.log(time);
+  // });
+
+  // var options = { 
+  //   method: 'GET',
+  //   url: API_URL + 'v1/ticker',
+  //   qs: { 
+  //     markets: marketList[0].market
+  //   } 
+  // };
+
+  // request(options, function (error, response, body) {
+  //   if (error) throw new Error(error);
+
+  //   var data = JSON.parse(body);
+  //   console.log(data);
+  // });
+
+}
+
+function loadLogofMonthRaiseCount(market, time) {
+  if (time == null) {
+    console.log('===========' + market + '===========');
+  }
+  var options = { 
+    method: 'GET',
+    url: API_URL + 'v1/candles/minutes/1',
+    qs: { 
+      market: market,
+      to: time,
+      count: 200
+    } 
+  };
+
+  request(options, (error, response, body) => {
+    if (error) throw new Error(error);
+
+    var data = JSON.parse(body);
+    for (var i in data) {
+      var open = data[i].opening_price;
+      var finish = data[i].trade_price;
+
+      if (finish - open > 0) {
+        var result = (finish - open) / open * 100;
+        if (result > RAISE_RATE) {
+          console.log(data[i].candle_date_time_kst);
+        }
+      }
+    }
+    setTimeout(() => {
+      var nextTime = new Date(data[data.length - 1].candle_date_time_kst);
+
+      if (currentMarket < marketList.length - 1) {
+        var currentTime = new Date();
+        currentTime.setMonth(currentTime.getMonth() - 1);
+        if (currentTime.getTime() < nextTime.getTime()) {
+          loadLogofMonthRaiseCount(market, nextTime.toISOString());
+        } else {
+          loadLogofMonthRaiseCount(marketList[++currentMarket].market, null);
+        }
+      }
+    }, 100);
+  });
 }
 
 
@@ -129,5 +221,5 @@ function completeLoadInfo() {
 
 
 app.listen(80, function() {
-  console.log('Conneted 80 port!');
+  console.log('Conneted 80 port!\n');
 });
